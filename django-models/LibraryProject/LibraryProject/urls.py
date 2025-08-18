6448c2b4380d922b44be5ade925ce577d6c38b7f
onyenike <onyenikefestus2019@gmail.com>
2025-08-18 16:33:25 -0700

    Update urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("books/", include("bookshelf.urls")),
@@ -24,4 +25,5 @@ urlpatterns = [
     path('', include('relationship_app.urls')),
 
 ]
+
 
