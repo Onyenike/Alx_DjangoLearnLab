43f2c7fc9a1fbac341ced500b0ed86a2a54d0606
onyenike <onyenikefestus2019@gmail.com>
2025-08-18 15:14:29 -0700

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def library_detail(request):
+    return render(request, 'library_detail.html')
+
+def list_books(request):
+    return render(request, 'list_books.html')
+
