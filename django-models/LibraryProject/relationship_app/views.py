8a2eb7267422c86af603090b2ba0e70e9a1f97c5
onyenike <onyenikefestus2019@gmail.com>
2025-08-19 15:27:55 -0700

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,12 +5,13 @@ from  . models import Library
 def library_detail(request):
     tours = Library.objects.all()
     context = {'tours':tours}
-    return render(request, "relationship_app/library_books.html")
+    return render(request, "relationship_app/library_details.html")
 
 def list_books(request):
     books = Book.objects.all()
     context = {'book':books}
     return render(request, "relationship_app/list_books.html")
+
 
 
 
