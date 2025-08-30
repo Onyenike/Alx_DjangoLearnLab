d4e9719d8a8d652abe5ffd01335f90bce4a6edb8
onyenike <onyenikefestus2019@gmail.com>
2025-08-30 10:38:41 -0700

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -40,9 +40,6 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     "rest_framework.authtoken",
-    "rest_framework.authentication.TokenAuthentication",
-    "rest_framework.permissions.IsAuthenticated",
-
 ]
 
 MIDDLEWARE = [
@@ -126,6 +123,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 
 
 
