570c3d8e9a6cf20b01e1f14000cf8d211b0c5827
onyenike <onyenikefestus2019@gmail.com>
2025-08-30 10:30:34 -0700

    Update settings.py

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'api',
     'rest_framework',
     "rest_framework.authtoken",
-    "rest_framework.authentication.TokenAuthentication".
+    "rest_framework.authentication.TokenAuthentication",
+    "rest_framework.permissions.IsAuthenticated",
 
 ]
 
@@ -125,5 +126,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
