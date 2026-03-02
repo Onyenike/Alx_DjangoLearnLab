d6297ba7f0833074f5bb2b9a4046ea67ced4e9b7
onyenike <onyenikefestus2019@gmail.com>
2026-03-02 13:42:24 -0800

    Add files via upload

--- advanced-api-project/api/models.py
+++ advanced-api-project/api/models.py
@@ -2,28 +2,15 @@ from django.db import models
 
 # Create your models here.
 class Author(models.Model):
-    """
-    Represents an author who can have multiple books.
-    Fields:
-        - name: The author's full name (string).
-    """
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
 
-
 class Book(models.Model):
-    """
-    Represents a book written by an author.
-    Fields:
-        - title: The book's title.
-        - publication_year: Year the book was published.
-        - author: ForeignKey to Author (One-to-Many relationship).
-    """
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=255)
     publication_year = models.IntegerField()
-    author = models.ForeignKey(Author, related_name="books", on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='books')
 
     def __str__(self):
-        return f"{self.title} ({self.publication_year})"
+        return self.title
--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Book, Author
+from .serializers import BookSerializer,AuthorSerializer
 
-# Create your views here.
+class BookListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class AuthorListCreateAPIView(generics.ListCreateAPIView):
+    queryset = Author.objects.all()
+    serializer_class = AuthorSerializer
